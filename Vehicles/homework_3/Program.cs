4d9f8032c24d7455ba67262c180b61217371ceca
unknown <rotemabir25@gmail.com>
2021-02-15 15:47:07 +0200

    codes

---
+++ Vehicles/homework_3/Program.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_3
+{
+    public enum GazType
+    {
+        cooking_gaz,
+        soler,
+        benzin,
+    }
+    public class Vehicle
+    {
+        private int rishuy;
+        private GazType gaz_type;
+
+        public Vehicle(int _rishuy, GazType _gaz_type)
+        {
+            rishuy = _rishuy;
+            gaz_type = _gaz_type;
+        }
+        public int Rishuy
+        {
+            get { return rishuy; }
+
+        }
+        public GazType Gaz_Type
+        {
+            get { return gaz_type; }
+
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("vehicle#{0}", rishuy);
+            Console.WriteLine("rides on{0}", gaz_type);
+        }
+        public static GazType ParseGazType(string str)
+        {
+
+            if (str == "COOKING_GAZ")
+            {
+                return GazType.cooking_gaz;
+            }
+            else if (str == "BENZIN")
+            {
+                return GazType.benzin;
+            }
+            else if (str == "SOLER")
+            {
+                return GazType.soler;
+            }
+
+            else
+            {
+                throw new Exception(string.Format("Can not parse value {0} into a GasType", str));
+            }
+
+        }
+        public static bool Parse(string str, ref Vehicle res)
+        {
+            string[] stam = { "|" };
+            string[] split = str.Split(stam, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < split.Length; i++)
+            {
+                split[i] = split[i].Trim();
+            }
+            long splitCount = split.LongCount();
+
+
+            if (splitCount < 2 || splitCount > 4)
+            {
+                res = null;
+                return false;
+            }
+
+            if (splitCount == 4)
+            {
+                if (split[0] == "-1")
+                {
+                    try
+                    {
+                        int rishuy = int.Parse(split[0]);
+                        GazType gt = ParseGazType(split[1]);
+                        string name = split[2];
+                        int lenght = int.Parse(split[3]);
+                        res = new Boat(gt, lenght, name, rishuy);
+                        return true;
+
+                    }
+                    catch (Exception)
+                    {
+                        res = null;
+                        return false;
+                    }
+                }
+            }
+            
+                if (splitCount == 3)
+                {
+                    try
+                    {
+                        int rishuy = int.Parse(split[0]);
+                        GazType gt = ParseGazType(split[1]);
+                        ConsoleColor cc = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), split[2]);
+                        res = new MotorCycle(rishuy, gt, cc);
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        res = null;
+                        return false;
+                    }
+                }
+
+                if (splitCount == 2)
+                {
+                    try
+                    {
+                        int rishuy = int.Parse(split[0]);
+                        GazType gt = ParseGazType(split[1]);
+                        res = new Vehicle(rishuy, gt);
+                        return true;
+
+                    }
+                    catch (Exception)
+                    {
+                        res = null;
+                        return false;
+                    }
+                }
+
+          
+            return true;
+        }
+    }
+
+    public class MotorCycle : Vehicle
+    {
+        private ConsoleColor color;
+        public MotorCycle(int _rishuy, GazType _gaz_type, ConsoleColor _color)
+               : base(_rishuy, _gaz_type)
+        {
+            color = _color;
+        }
+        public void Paint(ConsoleColor change)
+        {
+            color = change;
+        }
+        public override void Print()
+        {
+            Console.ForegroundColor = this.color;
+            base.Print();
+            Console.WriteLine("color:{0}", color);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    public class Boat : Vehicle
+    {
+        private int length;
+        private string name;
+
+        public Boat(GazType _gaz_type, int _length, string _name, int _rishuy) : base(-1, _gaz_type)
+        {
+            length = _length;
+            name = _name;
+        }
+        public int Length
+        {
+            get { return length; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public override void Print()
+        {
+
+            base.Print();
+            Console.WriteLine("boat name is {0}", name);
+            Console.WriteLine("boat length is:{0}", length);
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+
+        }
+    }
+    class Program
+    {
+        static bool Predicate (Boat b)
+        {
+            if (b.Length % 3 == 0)
+            {
+                return true;
+            }
+
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            int counterT = 0;
+            int counterF = 0;
+            Vehicle vr = new Vehicle(0, GazType.benzin);
+            List<Vehicle> VH = new List<Vehicle>();
+   
+           
+
+           StreamReader sr = new StreamReader(@"C:\Users\ety\Documents\Visual Studio 2015\Projects\homework_3\313234189_data.txt");
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                if (Vehicle.Parse(line, ref vr))
+                {
+                    counterT++;
+                    VH.Add(vr);
+
+                }                
+                else
+                {
+                    counterF++;
+                }
+                line = sr.ReadLine();
+            }
+          
+            Console.WriteLine("number of true:" + counterT);
+            Console.WriteLine("number of false:" + counterF);
+            List<Vehicle> vehicle_list = new List<Vehicle>();
+            List<Boat> ba = new List<Boat>();
+            List<MotorCycle> mc = new List<MotorCycle>();
+
+            foreach (Vehicle v in VH)
+            {
+               
+                if (v is MotorCycle)
+                {
+                    mc.Add((MotorCycle)v);
+                }
+                if (v is Boat)
+                {
+                    ba.Add((Boat)v);
+                    
+                }
+                else if (v is Vehicle)
+                {
+                   vehicle_list.Add((Vehicle)v);
+
+                }
+            }
+            Dictionary<int, int> motor = new Dictionary<int, int>();
+           
+         foreach (MotorCycle mtor in mc)
+            {
+                if(!motor.ContainsKey(mtor.Rishuy))
+                {
+                    motor.Add(mtor.Rishuy, 1);
+                }
+                else
+              {
+                    motor[mtor.Rishuy]++;
+                }
+            }
+            var list = motor.Keys.ToList();
+            list.Sort();
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\ety\Documents\Visual Studio 2015\Projects\homework_3\MyOutput.txt", false))
+            {
+                
+                sw.WriteLine("number of true:{0}", counterT);
+                sw.WriteLine("number of false:{0}", counterF);
+                ba.RemoveAll(Predicate);
+                sw.WriteLine("Length of remaining Boats={0}", ba.Count);
+                foreach (var item in list)
+                {
+                    sw.WriteLine(string.Format("{0}-{1}",item,motor[item]));
+                }
+            }
+
+        }
+    }
+}
---
+++ clothing/for tesr/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_tesr
+{
+    public enum Size
+    {
+        S,
+        M,
+        L,
+        XL,
+    }
+    abstract public class Item
+    {
+        private double price;
+        private string name;
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0) { value = price; }
+                else { throw new Exception(); }
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { value = name; }
+        }
+        public Item(double _price, string _name)
+        {
+            price = _price;
+            name = _name;
+        }
+    }
+
+    public class Clothing_item : Item
+    {
+        private Size size;
+        public Size sizes
+        { get { return size; } }
+
+        public Clothing_item(Size _size, double _price, string _name) : base(_price, _name)
+        {
+            size = _size;
+        }
+    }
+
+    public class Electric_item: Item
+    {
+        private int power;
+        public int Power
+        { get { return power; } }
+
+        public Electric_item(int _power,double _price,string _name) : base(_price,_name)
+        {
+            power = _power;
+        }
+    }
+    public class Store
+    {
+        List<Clothing_item> clothes = new List<Clothing_item>();
+        Electric_item[] electrics = new Electric_item[100];
+
+        public int is_null(Electric_item [] electrics)
+        {
+            
+            for (int i = 0; i < electrics.Length; i++)
+            {
+                if (electrics[i] == null)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void add_electric()
+        {
+                if (is_null(electrics) == -1)
+                {
+                    Console.WriteLine("no place");
+                }
+                else
+                {
+                        Console.WriteLine("add new electric");
+                        Console.WriteLine("please enter name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("please enter price");
+                        double price = double.Parse(Console.ReadLine());
+                        Console.WriteLine("please enter power");
+                        int power = int.Parse(Console.ReadLine());
+                        Electric_item e = new Electric_item(power, price, name);
+           }
+        }
+        public void get_cheapest_electric(Electric_item[] electrics)
+        {
+            int cell =0 ;
+            double price = electrics[0].Price;
+            for (int i = 1; i < electrics.Length; i++)
+            {
+             if(price> electrics[i].Price)
+                {
+                    cell = i;
+                    price = electrics[i].Price;
+                }
+            }
+            Console.WriteLine("electric item: name={0},price={1},power={2}", electrics[cell].Name, electrics[cell].Price,electrics[cell].Power);
+        }
+
+        public void Remove_Cheap_Electric(int min_price)
+        {
+            for (int i = 0; i < electrics.Length; i++)
+            {
+                if(electrics[i].Price<min_price)
+                {
+                    electrics[i] = null;
+                }
+            }
+        }
+     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
